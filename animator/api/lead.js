ab5f004124331761e67e7674d10a7b7347e98d09
danilamashoshin <79203169+danilamashoshin@users.noreply.github.com>
2025-09-28 13:25:43 +0400

    Update lead.js

--- animator/api/lead.js
+++ animator/api/lead.js
@@ -1,6 +1,6 @@
-const { sendToFacebookCAPI } = require('./_lib.js');
+import { sendToFacebookCAPI } from './_lib.js';
 
-async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     res.status(204).end();
     return;
@@ -21,6 +21,3 @@ async function handler(req, res) {
 
   res.status(200).json({ ok: true });
 }
-
-module.exports = handler;
-
